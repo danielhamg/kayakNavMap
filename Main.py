10bce395745ee5379e0f6ca0419091be2d0c521e
Tommy Poa <Tommy@Tommys-MacBook-Pro.local>
2018-04-20 00:31:22 -0700

    Implementing Main.py.

--- Main.py
+++ Main.py
@@ -1,10 +1,32 @@
 from requests import get
+import time
 import json
 
+class Main:
+    def __init__:
+        file = get("https://services.marinetraffic.com/api/exportvessels/v:8/2bede1ff947de189f635a8781c0d606b455f61b4/timespan:2/protocol:json")
+        file = SampleData_4_9__10-09am.txt # Sample data; not sure if syntax is correct
+        jsonObj = json.loads(file.text)
 
-file = get("https://services.marinetraffic.com/api/exportvessels/v:8/2bede1ff947de189f635a8781c0d606b455f61b4/timespan:2/protocol:json")
-jsonObj = json.loads(file.text)
+        self.currentShips = Ships(jsonObj)
+        self.currentAhmet = Ahmet(40, 40) # Arbitrary
 
-ahmet = Ahmet(jsonObj, lat, lon)
+while true:
+    # Phase 1: Data gathering
+    mainRunner = Main()
 
-print(ahmet.getShipItem(1, "MMSI"))
+    # Phase 2A: Base filter of ships within radius
+    shipsWithinRadius = DataProcessing.withinRadiusShips(mainRunner.currentShips.allShips, mainRunner.currentAhmet)
+    print(shipsWithinRadius)
+
+    # Phase 2B: Direction filter
+    shipsDirectionTuple = DataProcessing.withinDirectionShips(shipsWithinRadius, ahmet)
+    shipsTowards = shipsDirectionTuple[0]
+    shipsAway = shipsDirectionTuple[1]
+    print(shipsTowards, shipsAway)
+
+    # Phase 3: Alert Ahmet if he presses the button
+
+
+    # REPEAT: Delay for 5 minutes
+    time.sleep(300)
