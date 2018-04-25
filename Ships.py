cf4b42bcc7818cce4dde5953e01e5c763f6a77f7
daniel.gultom <danielhamg@gmail.com>
2018-04-24 22:28:02 -0700

    changes in processing files

--- Ships.py
+++ Ships.py
@@ -1,64 +1,47 @@
 class Ships:
 
     # Ship Fields: Corresponding to JSON file from Marine Traffic API
+    #################################
+    # Previously under Ships class. Now will be simplified to only take in a ship array
+    # and a desired field
+    ################################
     fields = {
-        "SHIP_ID" : 2,
-		"LAT" : 3,
-		"LON" : 4,
-		"SPEED" : 5,
-		"HEADING" : 6,
-		"COURSE" : 7,
-		"STATUS" : 8,
-		"TIMESTAMP" : 9,
-		"DSRC" : 10,
-		"UTC_SECONDS" : 11
-	}
+            "SHIP_ID" : 2,
+            "LAT" : 3,
+            "LON" : 4,
+            "SPEED" : 5,
+            "HEADING" : 6,
+            "COURSE" : 7,
+            "STATUS" : 8,
+            "TIMESTAMP" : 9,
+            "DSRC" : 10,
+            "UTC_SECONDS" : 11
+        }
 
-    # Constructor: takes in JSON file
-    def __init__(self, file):
-        self.allShips = file
-
-    # Returns a tuple of predicted longitude and latitude (in that order)
-    def predictNextLatLon(ship, nexTime):
-        speedX = knotsToMps(cos(getSpeedShip(ship)))
-        speedY = knotsToMps(sin(getSpeedShip(ship)))
-        diffTime = timeDifferenceSeconds(getTimeShip(ship), nexTime)
-        diffX = mtoCoor(speedX * diffTime)    # should be in coor
-        diffY = mtoCoor(speedY * diffTime)    # should be in coor
-        newLon = getLonShip(ship) + diffX
-        newLat = getLatShip(ship) + diffY
-        return [newLon, newLat]
-
-
-    ######################################################
-    ################## HELPER FUNCTIONS ##################
-    ######################################################
-
-    # Gets the output of a particular ship's field; General helper function
-	def getShipField(self, shipIndex, field):
-		item = item.upper()
-		return self.allShips[index][fields[item]]
+    def getShipField(ship, field):
+        field = field.upper()
+        return ship[fields[field]]
 
     # Get a ship's ID
-    def getShipId(self, shipIndex):
-        return int(self.getShipfield(index, "SHIP_ID"))
+    def getShipId(ship):
+        return int(getShipField(ship, "SHIP_ID"))
 
-	# Get a ship's latitude
-	def getLatShip(self, shipIndex):
-		return int(self.getShipField(index, "LAT"))
+    # Get a ship's latitude
+    def getLatShip(ship):
+        return float(getShipField(ship, "LAT"))
 
-	# Get a ship's longtitude
-	def getLonShip(self, shipIndex):
-		return int(self.getShipField(index, "LON"))
+    # Get a ship's longtitude
+    def getLonShip(ship):
+        return float(getShipField(ship, "LON"))
 
-	# Get a ship's heading
-	def getHeadingShip(self, shipIndex):
-		return int(self.getShipField(index, "HEADING"))
+    # Get a ship's heading
+    def getHeadingShip(ship):
+        return int(getShipField(ship, "HEADING"))
 
     def getSpeedShip(ship):
         return float(getShipField(ship, "SPEED"))
 
-    def getSpeedTimeStamp(ship):
+    def getShipTimeStamp(ship):
         return getShipField(ship, "TIMESTAMP")
 
     def timeStampCompress(stamp):
@@ -66,7 +49,7 @@ class Ships:
         return stamp[indexT + 1:]
 
     def getTimeShip(ship):
-        return timeStampCompress(getSpeedTimeStamp(ship))
+        return timeStampCompress(getShipTimeStamp(ship))
 
     def timeDifferenceSeconds(before, after):
         FMT = '%H:%M:%S'
